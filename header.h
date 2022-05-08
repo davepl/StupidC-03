ad6f0ffe0ffdf4f238af81bd26fb98811d4a95b0
Dave Plummer <dave@daveandnikki.com>
2022-05-08 09:58:42 -0700

    Added format

---
+++ header.h
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stddef.h>
+
+#ifdef _WIN32
+    #include <Windows.h>
+#else
+    #include <unistd.h>
+#endif
+
+#include <ctime>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+#include <chrono>
+#include <limits>
+        
+#include <limits.h>
+#include <fmt/format.h>
+
+// If on old C, define __STDC_FORMAT_MACROS before including inttypes.h
+
+#include <inttypes.h>
--- main.cpp
+++ main.cpp
@@ -1,71 +1,97 @@
-#include <stdio.h>
-#include <unistd.h>
-
-#include <ctime>
-#include <iostream>
-#include <iomanip>
-#include <algorithm>
-#include <iostream>
-#include <iomanip>
-#include <ctime>
-#include <chrono>
-
-// If on old C, define __STDC_FORMAT_MACROS before including inttypes.h
-
-#include <inttypes.h>
-
-
+#include "header.h"
 
 int main(int argc, char ** argv)
 {
+        // What we can say about type sizes in C:
+        //
+        // char, signed char, and unsigned char are at least 8 bits
+        // signed short, unsigned short, signed int, and unsigned int are at least 16 bits
+        // signed long and unsigned long are at least 32 bits
+        // signed long long and unsigned long long are at least 64 bits        
 
         printf("Hello World\n");
 
+        // When we know the variable type size
+
+        int x = 3;
+        printf("x = %d\n", x);
+
+        // Sometimes we don't know the actual size, becasue of the "at least" stuff
+        // intmax_t is the largest type that your compiler and lib can deal with
+
         intmax_t maxt = 0xdeadbeef;
         printf("num64 as MaxSizeType == %jd\n", maxt);
-                
+
+        // Printing 64 bit quantities properly
+
+        static_assert(sizeof(intmax_t) >= sizeof(uint64_t));
+
         uint64_t num64;
-        
         num64 = 1LLU<<63;
-        printf("2^63 == %llu\n", num64);
+        printf("2^63 == %lu\n", num64);
 
         num64 = 0xdeadbeefbaadf00d;
-        printf("num64 == %llx\n", num64);
+        printf("0xdeadbeefbaadf00d == %lx\n", num64);
+        printf("0xdeadbeefbaadf00d as signed decimal == %" PRId64 "\n", num64);
+        printf("0xdeadbeefbaadf00d as unsigned decimal == %" PRIu64 "\n", num64);
 
-        printf("num64 as signed == %" PRId64 "\n", num64);
-
-        printf("num64 as unsigned == %" PRIu64 "\n", num64);
-
-        using namespace std;
-                        
-        cout << "num64 streamed out == " << num64 << endl;
+        using namespace std;      
+        cout << "num64 streamed to cout == " << num64 << endl;
         
         // __uint128_t num128 = 0;
         // cout << "num128 streamed out == " << num128 << endl;
         
+        //
+        // Durations in C++
+        //
+
         using namespace std::chrono;
         
         time_t result = time(nullptr);
         cout << ctime(&result);
         
-        auto t1 = std::chrono::high_resolution_clock::now();
+        auto t1 = high_resolution_clock::now();
         sleep(1);
-        auto t2 = std::chrono::high_resolution_clock::now();
+        auto t2 = high_resolution_clock::now();
 
         // floating-point duration: no duration_cast needed
-        std::chrono::duration<double, std::milli> fp_ms = t2 - t1;
+        duration<double, std::milli> fp_ms = t2 - t1;
 
         // integral duration: requires duration_cast
-        auto int_ms = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+        auto int_ms = duration_cast<milliseconds>(t2 - t1);
 
-        // converting integral duration to integral duration of shorter divisible time unit:
-        // no duration_cast needed
+        // Convert duration to different units
+
+        auto int_usec  = duration_cast<microseconds>(int_ms).count();
+        auto int_mssec = duration_cast<milliseconds>(int_ms).count();
+        auto int_nsec  = duration_cast<nanoseconds>(int_ms).count();
+
+        std::cout << "Sleeping for one second took " << fp_ms.count() << " ms, " << endl
+                  << "or " << int_mssec << " milliseconds" << endl
+                  << "or " << int_usec  << " microseconds" << endl 
+                  << "or " << int_nsec  << " nanoseconds" << endl;               
+
+        // Integer limits
+
         
-        std::chrono::duration<long, std::micro> int_usec = int_ms;
+        int min_int = INT_MIN;
+        int max_int = INT_MAX;
 
-        std::cout << "sleepine one seconds took " << fp_ms.count() << " ms, "
-                  << "or " << int_ms.count() << " whole milliseconds "
-                  << "(which is " << int_usec.count() << " whole microseconds)" << std::endl;
-                  
+        min_int = numeric_limits<int>::min();
+        max_int = numeric_limits<int>::max();
+        cout << "Min: " << min_int << ", Max: " << max_int << endl;
+
+        // Formatting
+        //
+        // cmake CMakeLists.txt
+        // make -j 64
+        // sudo make install
+        // use clang++
+        // add -lfmt to compiler command line
+
+        using namespace fmt::v8;
+
+        cout << format("Hello");
+
         return 0;
 }
